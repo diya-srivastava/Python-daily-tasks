ebc8c092541619888c34bac948af6f370f07fcee
diya-srivastava <diyasrivastava2308@gmail.com>
2026-06-25 14:37:30 +0530

    Add hangman game part 2

---
+++ hangmangame_part2.py
@@ -0,0 +1,17 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+placeholder = ""
+for position in range(1,9):
+    placeholder += "_"
+print(placeholder)
+
+guess = input("Guess a letter: ").lower()
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
